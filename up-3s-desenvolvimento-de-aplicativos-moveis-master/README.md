111a6cd12e34e43e9232c9c6f49fdf74fb48ed60
Carlos Henrique <carloss.chac@gmail.com>
2022-11-24 12:52:58 -0300

    commit

--- up-3s-desenvolvimento-de-aplicativos-moveis-master/README.md
+++
@@ -1,3 +0,0 @@
-# up-3s-desenvolvimento-de-aplicativos-moveis
-Universidade Positivo (3° Período)
-Ministrada pelo professor Diego Palma Navarro (palma1607)
--- up-3s-desenvolvimento-de-aplicativos-moveis-master/lista_de_presentes/app/src/main/java/br/com/up/listadepresentes/models/Gift.kt
+++
@@ -1,6 +0,0 @@
-package br.com.up.listadepresentes.models
-
-data class Gift(var name:String,
-                var giftName:String,
-                var description:String
-)
